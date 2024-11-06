d29aa4626cfcafe6b3e557f66cce4e3cf53d4797
Clemens Farnleitner <clemens.farnleitner@student.tugraz.at>
2024-11-06 10:46:27 +0100

    Adding Second Level

--- main.py
+++ main.py
@@ -8,29 +8,61 @@ soup = BeautifulSoup(response.content, "html.parser")
 # Finding the main questions container
 questions_container = soup.find("div", id="questions")
 
-# List to store all question dictionaries
-questions_data = []
+# Dictionaries to store summary and detailed question data
+questions_summary = {}
+questions_details = {}
 
 # Loop through each question summary
 for question in questions_container.find_all("div", class_="s-post-summary"):
-    # Extract the necessary information and store it in a dictionary
-    question_data = {
-        "post_id": question.get("data-post-id"),
-        "votes": question.find("span", class_="s-post-summary--stats-item-number").text,
-        "answers": question.find("div", class_="has-answers").find("span", class_="s-post-summary--stats-item-number").text if question.find("div", class_="has-answers") else "0",
-        "views": question.find("div", title=lambda x: x and "views" in x).find("span", class_="s-post-summary--stats-item-number").text,
-        "title": question.find("h3", class_="s-post-summary--content-title").find("a").text,
-        "link": "https://stackoverflow.com" + question.find("h3", class_="s-post-summary--content-title").find("a")['href'],
-        "excerpt": question.find("div", class_="s-post-summary--content-excerpt").text.strip(),
-        "tags": [tag.text for tag in question.find("div", class_="s-post-summary--meta-tags").find_all("a", class_="s-tag")],
-        "user_name": question.find("div", class_="s-user-card--info").find("a").text if question.find("div", class_="s-user-card--info") else "Unknown",
-        "user_rep": question.find("div", class_="s-user-card--info").find("li", class_="s-user-card--rep").text if question.find("div", class_="s-user-card--info") else "N/A",
+    post_id = question.get("data-post-id")
+    votes = question.find("span", class_="s-post-summary--stats-item-number").text
+    answers = question.find("div", class_="has-answers").find("span", class_="s-post-summary--stats-item-number").text if question.find("div", class_="has-answers") else "0"
+    views = question.find("div", title=lambda x: x and "views" in x).find("span", class_="s-post-summary--stats-item-number").text
+    title_element = question.find("h3", class_="s-post-summary--content-title").find("a")
+    title = title_element.text
+    link = "https://stackoverflow.com" + title_element['href']
+    excerpt = question.find("div", class_="s-post-summary--content-excerpt").text.strip()
+    tags = [tag.text for tag in question.find("div", class_="s-post-summary--meta-tags").find_all("a", class_="s-tag")]
+    user_info = question.find("div", class_="s-user-card--info")
+    user_name = user_info.find("a").text if user_info else "Unknown"
+    user_rep = user_info.find("li", class_="s-user-card--rep").text if user_info else "N/A"
+    time_element = question.find("time", class_="s-user-card--time")
+
+    # Store summary in dictionary using post_id as key
+    questions_summary[post_id] = {
+        "votes": votes,
+        "answers": answers,
+        "views": views,
+        "title": title,
+        "link": link,
+        "excerpt": excerpt,
+        "tags": tags,
+        "user_name": user_name,
+        "user_rep": user_rep
     }
     
-    # Append the dictionary to the list
-    questions_data.append(question_data)
+    # Fetch and parse the question details page
+    question_response = requests.get(link)
+    question_soup = BeautifulSoup(question_response.content, "html.parser")
 
-# show how many questions have been found 
-print(len(questions_data))
-for question in questions_data:
-    print(question, "\n")
+    metadata_container = question_soup.find("div", class_="d-flex fw-wrap pb8 mb16 bb bc-black-200")
+    date_created = metadata_container.find("time", itemprop="dateCreated")['datetime'] if metadata_container.find("time", itemprop="dateCreated") else None
+    last_activity_element = metadata_container.find("a", href="?lastactivity")
+    last_activity_date = last_activity_element['title'] if last_activity_element else None
+    view_count = int(question_soup.find("div", class_ ="flex--item ws-nowrap mb8")['title'].split()[1])
+    
+
+
+
+
+
+    questions_details = {
+        "date_created": date_created,
+        "last_activity_date": last_activity_date,
+        "view_count": view_count
+    }
+
+    break
+
+print(questions_summary)
+print(questions_details)
